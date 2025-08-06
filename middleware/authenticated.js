59c9a90047ead6295b67b5f0970e11fa74766a35
Jeremy Gervais <jeremy@Jeremys-MacBook-Pro-2.local>
2025-08-06 19:14:08 -0400

    prepare for v2

--- middleware/authenticated.js
+++
@@ -1,6 +0,0 @@
-export default function(context) {
-  // If the user is not authenticated
-  if (!context.store.state.users.user && context.route.path[0] !== '/') {
-    return context.redirect('/')
-  }
-}
